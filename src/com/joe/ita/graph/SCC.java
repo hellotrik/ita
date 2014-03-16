747bd3e8ad6a677f1caacc7b4e103b2772749cc2
Joe <zhoujie338@126.com>
2014-03-16 18:14:24 +0800

    refactor the code

--- src/com/joe/ita/graph/SCC.java
+++ src/com/joe/ita/graph/SCC.java
@@ -1,24 +1,22 @@
 package com.joe.ita.graph;
 
-import java.util.List;
 import java.util.Map;
 
 public class SCC {
 
     public void stronglyConnectedComponents(Graph g) {
-//        TopologicalSort ts = new TopologicalSort();
-//        List<Vertex> tsList = ts.topologicalSort(g);
-//        System.out.println(tsList);
-        
+        // TopologicalSort ts = new TopologicalSort();
+        // List<Vertex> tsList = ts.topologicalSort(g);
+        // System.out.println(tsList);
+
         DFS dfs1 = new DFS();
         dfs1.depthFirstSearch(g);
         Map<Vertex, Integer> topoSortMap = dfs1.getFinshTime();
-        
 
         Graph gT = ITAUtils.transpose(g);
         // System.out.println(gT + "\n" + "gT's vertices:" + gT.getVertexNum() +
         // ";gT's edges:" + gT.getEdgeNum());
-        
+
         DFS dfs2 = new DFS();
         dfs2.depthFirstSearchByTopoSort(gT, topoSortMap);
         System.out.println(dfs2.getParent());
