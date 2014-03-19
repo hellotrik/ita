a9e8f1d60bce9abb1a771f5af5d0dd9b3258f493
Joe <zhoujie338@126.com>
2014-03-19 11:00:02 +0800

    add basic functions for single source shortest path.

---
+++ src/com/joe/ita/graph/SingleSourceShortestPaths.java
@@ -0,0 +1,29 @@
+package com.joe.ita.graph;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SingleSourceShortestPaths {
+
+    Map<Vertex, Vertex> parentMap = new HashMap<Vertex, Vertex>();
+
+    /* Upper bound on the weight of a shortest path from source s to v. */
+    Map<Vertex, Integer> distanceMap = new HashMap<Vertex, Integer>();
+
+    public void initSingleSource(Graph g, Vertex s) {
+        for (Vertex v : g.getVertices()) {
+            parentMap.put(v, null);
+            distanceMap.put(v, Integer.MAX_VALUE);
+        }
+
+        distanceMap.put(s, 0);
+    }
+
+    public void relax(Vertex u, Vertex v, int w) {
+        if (distanceMap.get(v) > distanceMap.get(u) + w) {
+            distanceMap.put(v, distanceMap.get(u) + w);
+            parentMap.put(v, u);
+        }
+    }
+
+}
